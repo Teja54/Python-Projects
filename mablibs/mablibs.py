449554ab4cae2598117e0cc30628b77be401cfee
Devisetti Teja <71646126+Teja54@users.noreply.github.com>
2021-07-19 10:41:02 +0530

    Add files via upload

---
+++ mablibs/mablibs.py
@@ -0,0 +1,8 @@
+color = input("Enter a color? ")
+noun = input("Enter a noun? ")
+person = input("Enter a person's name? ")
+
+
+print("Roses are {}".format(color))
+print("{} is sweet".format(noun))
+print("I love {}".format(person))
